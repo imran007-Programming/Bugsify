0698fd1371394c397fe47ac45ac1d3142e1a1f6c
Imran Hasan <imranphero@gmail.com>
2026-07-01 21:10:33 +0600

    added comment module

--- Server/src/comments/comments.service.ts
+++ Server/src/comments/comments.service.ts
@@ -1,4 +1,20 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
-export class CommentsService {}
+export class CommentsService {
+    constructor(private prisma: PrismaService) { }
+    async createComment(userId: string, bugId: string, payload: any) {
+        const comment = await this.prisma.comment.create({
+            data: {
+                userId,
+                bugId,
+                ...payload
+            }
+        })
+        return {
+            message: "comment created successfully",
+            comment
+        }
+    }
+}
